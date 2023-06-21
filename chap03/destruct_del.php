5d10d92dd65b57f58190f47c653ffa4873da441b
hamakachu <mentaiko0202@yahoo.co.jp>
2023-06-21 21:15:56 +0900

    Update destruct_del.php

--- chap03/destruct_del.php
+++ chap03/destruct_del.php
@@ -4,3 +4,8 @@ $data = [1,2,3,4,5];
 print $a;
 print $b;
 print $c;
+
+$map = ['title' => 'Python', 'price' => '3000'];
+['title' => $title, 'price' => $price] = $map;
+print $title;
+print $price;
